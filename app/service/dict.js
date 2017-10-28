463ab256dd83536405451d219d54d96938cdc750
huzl <email@example.com>
2017-10-28 11:03:16 +0800

    add js

--- app/service/dict.js
+++ app/service/dict.js
@@ -3,48 +3,56 @@
  */
 module.exports = app => {
     class DictService extends app.Service {
-        async get_dict_array(token) {
-            const  url = this.app.config.api.get_api_dict_type();
-            const result = await this.app.curl(`${url}`, {
+
+        async request(url, token, opts) {
+            opts = Object.assign({
+                timeout: [ '30s', '30s' ],
                 method: 'GET',
                 dataType: 'json',
+                contentType: 'json',
                 headers: { Authorization: `Bearer ${token}`}
-            });
+            }, opts);
+            return await this.ctx.curl(`${url}`, opts);
+        }
+
+        async get_dict_array(token) {
+            const  url = this.app.config.api.get_api_dict_type();
+            const result = await this.request(url, token, {});
+            this.checkSuccess(result);
             return result;
         }
 
         async get_dict_edit(token, id) {
             const  url = this.app.config.api.get_api_dict_type_single(id);
-            const result = await this.app.curl(`${url}`, {
-                method: 'GET',
-                dataType: 'json',
-                headers: { Authorization: `Bearer ${token}`}
-            });
+            const result = await this.request(url, token, {});
+            this.checkSuccess(result);
             return result;
         }
 
         async update_baseType(token, params) {
             const  url = this.app.config.api.put_api_baseType();
-            const result = await this.app.curl(`${url}`, {
-                method: 'PUT',
-                dataType: 'json',
-                contentType: 'json',
-                data: params,
-                headers: { Authorization: `Bearer ${token}`}
-            });
+            const opt = {data: params, method: 'PUT'};
+            const result = await this.request(url, token, opt);
+            this.checkSuccess(result);
             return result;
         }
 
         async create_baseType(token, params) {
             const  url = this.app.config.api.post_api_baseType();
-            const result = await this.app.curl(`${url}`, {
-                method: 'POST',
-                dataType: 'json',
-                contentType: 'json',
-                data: params,
-                headers: { Authorization: `Bearer ${token}`}
-            });
+            const opt = {data: params, method: 'POST'};
+            const result = await this.request(url, token, opt);
+            this.checkSuccess(result);
             return result;
+        }
+
+        checkSuccess(result) {
+            if (result.status !== 200) {
+                const errorMsg = result.data && result.data.error_msg ? result.data.error_msg : 'unknown error';
+                this.ctx.throw(result.status, errorMsg);
+            }
+            /*if (!result.data.success) {
+                this.ctx.throw(500, 'remote response error', { data: result.data });
+            }*/
         }
     }
 
